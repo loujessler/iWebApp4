1f9ca3c79c4dbaac76adb4488b8f1342d1727c41
loujessler <danilin055@yahoo.com>
2023-10-19 19:15:23 +0400

    added migrations_pending func to run.py

--- back/run.py
+++ back/run.py
@@ -1,12 +1,11 @@
-import time
 import os
+import time
 from django.db import connections
 from django.db.utils import OperationalError
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 application = get_wsgi_application()
-
 
 def check_database_connection():
     db_conn = None
@@ -21,13 +20,20 @@ def check_database_connection():
         if db_conn:
             db_conn.close()
 
+def migrations_pending():
+    output = os.popen("python manage.py showmigrations --list").read()
+    return "[ ]" in output
 
 if __name__ == "__main__":
     while True:
         if check_database_connection():
             print("Database available!")
-            os.system(
-                "python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000")
+
+            if migrations_pending():
+                os.system("python manage.py makemigrations")
+                os.system("python manage.py migrate")
+
+            os.system("python manage.py runserver 0.0.0.0:8000")
             break
         else:
             print("Waiting for database availability...")
